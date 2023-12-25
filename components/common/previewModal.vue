a69ce31e6d993e3aa30193fef8538e5ac5b634d2
zoujianjun <1042190903@qq.com>
2023-12-25 09:44:26 +0800

    【功能修改】提交视频预览框

---
+++ components/common/previewModal.vue
@@ -0,0 +1,53 @@
+<template>
+	<u-modal class="cust-modal" title="预览" :show="show" confirmText="关闭" @confirm="confirm" >
+		<view class="slot-content">
+			<video id="previewVideo" class="video" :src="src" controls autoplay />
+		</view>
+	</u-modal>
+</template>
+
+<script>
+	export default {
+		props: {
+			show: {
+				type: Boolean,
+				default: false
+			},
+			src: {
+				type: String,
+				default: ''
+			}
+		},
+		data() {
+			return {
+				videoContext: null,
+			}
+		},
+		watch: {
+			show(newVal) {
+				this.videoContext = uni.createVideoContext('previewVideo', this);
+				newVal && this.videoContext && this.videoContext.play();
+			}
+		},
+		methods: {
+			// 关闭
+			confirm() {
+				this.$emit('closeModal');
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.cust-modal /deep/ .u-modal__title {
+		margin-bottom: 0;
+	}
+	.slot-content {
+		width: 100%;
+
+		.video {
+			width: 100%;
+			height: 200px;
+		}
+	}
+</style>
